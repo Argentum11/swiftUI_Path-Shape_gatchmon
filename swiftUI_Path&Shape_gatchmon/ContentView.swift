1caa18144aab41155f1e69842d02aab53dde75ae
Leo <00957050@email.ntou.edu.tw>
2021-10-19 19:54:49 +0800

    Initial Commit

---
+++ swiftUI_Path&Shape_gatchmon/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftUI_Path&Shape_gatchmon
+//
+//  Created by User22 on 2021/10/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftUI_Path&Shape_gatchmon/swiftUI_Path_Shape_gatchmonApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_Path_Shape_gatchmonApp.swift
+//  swiftUI_Path&Shape_gatchmon
+//
+//  Created by User22 on 2021/10/19.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI_Path_Shape_gatchmonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
